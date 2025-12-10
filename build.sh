b5817b18dcffaf256f0429453bab4aaa60750328
Oriere Emmanuel <orieremmanuel@gmail.com>
2025-12-10 21:34:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "=== Installing dependencies ==="
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+echo "=== Collecting static files ==="
+python manage.py collectstatic --noinput --clear
+
+echo "=== Running migrations ==="
 python manage.py migrate
+
+echo "=== Build complete ==="
